d9ea4ed3d51c8cd2768399bcb0553558c173fafd
me-coderaider <kashyappankajnsp@gmail.com>
2024-07-05 23:20:31 +0530

    Spring JDBC - Insert, Delete & Select

--- src/main/java/rtg/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/rtg/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import rtg.learn_jpa_and_hibernate.course.Course;
+
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
@@ -12,7 +14,14 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert();
+		repository.insert(new Course(1,"Learn AWS","Pankaj"));
+		repository.insert(new Course(2,"Learn Azure","Pankaj"));
+		repository.insert(new Course(3,"Learn GCloud","Pankaj"));
+		repository.insert(new Course(4,"Learn Java","Pankaj"));
+		
+		repository.deleteById(1);
+		
+		System.out.println(repository.findById(3));
 	}
 
 	
--- src/main/java/rtg/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRespository.java
+++ src/main/java/rtg/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRespository.java
@@ -1,8 +1,11 @@
 package rtg.learn_jpa_and_hibernate.course.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import rtg.learn_jpa_and_hibernate.course.Course;
 
 @Repository
 public class CourseJdbcRespository {
@@ -12,10 +15,28 @@ public class CourseJdbcRespository {
 	
 	private static String INSERT_QUERY="""
 			insert into course (id, name, author)
-			values(1,'Learn Spring', 'Pankaj');
+			values(?,?,?);
 			""";
 	
-	public void insert() {
-		springJdbcTemplate.update(INSERT_QUERY);
+	private static String DELETE_QUERY="""
+				delete from course where id = ? 
+			""";
+	
+	private static String SELECT_QUERY="""
+				select * from course where id =?
+			""";
+	
+	public void insert(Course course) {
+		springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+	}
+	
+	public void deleteById(long id) {
+		springJdbcTemplate.update(DELETE_QUERY, id);
+	}
+	
+	public Course findById(long id) {
+		// RESULT set map it to a Bean, and those components are called RowMappers
+		// Id as input
+		return springJdbcTemplate.queryForObject(SELECT_QUERY, new BeanPropertyRowMapper<>(Course.class),id);
 	}
 }
